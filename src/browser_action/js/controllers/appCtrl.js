cd24c183635dcfaae5a0d7b461c6c3c154e68693
PerryPan <plrthink@gmail.com>
2013-05-09 17:13:48 +0800

    add complete and ignore feature

--- src/browser_action/js/controllers/appCtrl.js
+++ src/browser_action/js/controllers/appCtrl.js
@@ -9,4 +9,22 @@ app.controller('appCtrl', function AlfredCtrl($scope, courseService) {
   $scope.openDeadlineInNewTab = function(deadline){
     chrome.tabs.create({'url': deadline.link});
   };
+
+  $scope.removeDeadline = function(deadline){
+    var rmed_deadlines = JSON.parse(localStorage.getItem('rmed_deadlines'));
+    rmed_deadlines.push(deadline);
+    localStorage.setItem('rmed_deadlines', JSON.stringify(rmed_deadlines));
+  };
+
+  $scope.hideRemovedDeadline = function(deadlines){
+   var rmed_deadlines_times = JSON.parse(localStorage["rmed_deadlines"])
+                           .map(function(deadline){
+                            return deadline.time;
+                           });
+   var undo_deadlines = deadlines.filter(function(deadline){
+    return -1 == rmed_deadlines_times.indexOf(deadline.time);
+   });
+   return undo_deadlines;
+  }
+
 });
